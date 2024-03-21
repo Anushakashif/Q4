a9629e2de8a70a7af4b8df00cb4f4e12af8888cb
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 14:28:57 -0700

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,6 @@
+"use strict";
+// 3. Famous Quote : Find a famous person you admire. Print the quote and the name ot its author. Your output should look something like this 
+// following, including the quotation marks :
+// Albert Einstein once said , “ A person who never made a mistake never tried anything new.” 
+let name1 = ("William Shakespeare said");
+console.log(`${name1}, “To be or not to be, that is the question”`);
